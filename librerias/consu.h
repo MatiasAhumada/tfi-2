9d491b7b6c7cdafe10534eefb04fff6be4daefc2
Matiasahumada <pagosptcmatias@gmail.com>
2024-01-20 18:53:52 -0300

    cree varios archivos para dar lineamiento al proyecto

--- librerias/consu.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-void hola(){
-    printf("hola mundo");
-}
--- recepcion.cpp
+++ recepcion.cpp
@@ -2,7 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <recep.h>
-
 main(){
- hola();
+     int opc;
+ do{
+   opc=menu();
+   _flushall();
+    switch(opc){
+            case 1:
+					
+					break;
+			case 2:
+                    
+					pacientes(paci,pac);
+					break;
+			case 3:
+					
+					break;
+			case 4: 		
+					
+					break;
+			case 5:
+					printf("\nHASTA PRONTO\n");
+					break;			
+			default:
+					printf("\nNo es una opcion correcta...");
+					break;
+    }
+    system("pause");
+ }while(opc!=5);
 }
